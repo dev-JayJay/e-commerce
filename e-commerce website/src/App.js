18f88c1f6af0192cc8121c25b8de95dd1ca0152b
dev-jay-jay <johngratia2@gmail.com>
2024-06-28 14:10:57 +0100

    products slides

---
+++ e-commerce website/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ProductList from './components/e-commerce/productList/ProductList';
+import NavBar from './components/e-commerce/navbar/NavBar'
+function App() {
+  return (
+    <div className="App">
+      
+      <NavBar />
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
